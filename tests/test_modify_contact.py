88a6b933aecb4fc9ef2a0949a2ecf00b86c970ca
Aspergean <unknownasperger@gmil.com>
2021-11-26 02:07:31 +0300

    Fixture validation

--- tests/test_modify_contact.py
+++ tests/test_modify_contact.py
@@ -2,12 +2,9 @@ from model.contact import Contact
 
 
 def test_amend_first_contact_firstname(app):
-        app.session.login(username="admin", password="secret")
         app.contact.amend_first_contact(Contact(firstname="Norm"))
-        app.session.logout()
 
 def test_amend_first_contact_lastname(app):
-        app.session.login(username="admin", password="secret")
         app.contact.amend_first_contact(Contact(lastname="McDormann"))
-        app.session.logout()
+
 
