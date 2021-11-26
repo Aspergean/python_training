ea131528d079fda9d830179b48693052f73feb50
Aspergean <unknownasperger@gmil.com>
2021-11-26 03:14:06 +0300

    Validation for modify group and contact tests if no groups and contacts in the system

--- tests/test_modify_contact.py
+++ tests/test_modify_contact.py
@@ -2,9 +2,13 @@ from model.contact import Contact
 
 
 def test_amend_first_contact_firstname(app):
+        if app.contact.count() == 0:
+                app.contact.create(Contact(firstname="test", lastname="test2"))
         app.contact.amend_first_contact(Contact(firstname="Norm"))
 
 def test_amend_first_contact_lastname(app):
+        if app.contact.count() == 0:
+                app.contact.create(Contact(firstname="test1", lastname="test3"))
         app.contact.amend_first_contact(Contact(lastname="McDormann"))
 
 
