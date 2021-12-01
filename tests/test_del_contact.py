e0cd21ff9e88375e5930e873c6576c2283b199d6
Aspergean <unknownasperger@gmil.com>
2021-12-01 03:30:41 +0300

    Updated models and del_tests with comparing objects and lists

--- tests/test_del_contact.py
+++ tests/test_del_contact.py
@@ -1,9 +1,11 @@
 from model.contact import Contact
 
-def test_delete_first_group(app):
+def test_delete_first_contact(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname="test", middlename="test1", lastname="test2"))
     old_contacts = app.contact.get_contact_list()
     app.contact.delete_first_contact()
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) - 1 == len(new_contacts)
+    old_contacts[0:1] = []
+    assert old_contacts == new_contacts
