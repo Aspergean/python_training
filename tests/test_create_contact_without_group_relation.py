0a39ec87915d7e108be41a11e19d72c94c55a899
Aspergean <unknownasperger@gmil.com>
2021-12-07 12:23:05 +0300

    Added data driven for contact creation

--- tests/test_create_contact_without_group_relation.py
+++ tests/test_create_contact_without_group_relation.py
@@ -4,7 +4,6 @@ import pytest
 import random
 import string
 
-
 def random_string(prefix, maxlen):
     symbols = string.ascii_letters + string.digits + " "*5
     s = prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
@@ -19,8 +18,7 @@ def random_phone(maxlen):
 testdata = [
     Contact(email="1@none.st", email2="hyrray@test.ry", email3="lilu@rt.by",
             homepage="lolali.com", birth_day="6", birth_month="November", birth_year="1989",
-            aniv_day="13", aniv_month="June", aniv_year="2008", address2="Sydney, Last Stand av. 657",
-            phone2="+334234324324242", notes="Stop",
+            aniv_day="13", aniv_month="June", aniv_year="2008",
     firstname = (random_string("firstname", 15)),
     middlename = (random_string("middlename", 20)),
     lastname = (random_string("lastname", 20)),
@@ -31,7 +29,10 @@ testdata = [
     home_phone = ("", random_phone(20)),
     mobile_phone = ("", random_phone(20)),
     work_phone = ("", random_phone(20)),
-    fax = ("", random_phone(20)))
+    fax = ("", random_phone(20)),
+    address2 = ("", random_string("address2", 50)),
+    phone2 = ("", random_phone(20)),
+    notes = ("", random_string("notes", 100)))
     for i in range(2)
            ]
 
