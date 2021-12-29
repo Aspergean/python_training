0d26ee7102b662d5b8a3f0a9d3534c65e98563f0
Aspergean <UnknownAsperger1>
2021-12-29 18:48:38 +0300

    ORM fixture is added

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,16 +1,12 @@
-from fixture.db import DbFixture
+from fixture.orm import ORMFixture
+from model.group import Group
 
-db = DbFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
 try:
-    #cursor = connection.cursor()
-    #cursor.execute("select * from group_list")
-    #for row in cursor.fetchall():
-        #print(row)
-    contacts = db.get_contact_list()
-    for contact in contacts:
-        print(contact)
-    print(len(contacts))
+    l = db.get_contacts_not_in_group(Group(id=58))
+    for item in l:
+        print(item)
+    print(len(l))
 finally:
-    #connection.close()
-    db.destroy()
+    pass
