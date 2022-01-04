d3e60d89ddf475e3b7000dd867463a2330046283
Aspergean <UnknownAsperger1>
2022-01-04 16:15:21 +0300

    Updated test for adding and removing contact in group

--- tests/test_add_remove_contact_to_from_group.py
+++ tests/test_add_remove_contact_to_from_group.py
@@ -13,11 +13,11 @@ def test_add_contact_to_group(app):
     if len(orm.get_contacts_not_in_group(group)) == 0:
         app.contact.create(Contact(firstname="NewFirstname", lastname="NewLastname"))
     contact = random.choice(orm.get_contacts_not_in_group(group))
-    group_before = list(orm.get_contacts_in_group(group))
+    contacts_in_group_before = list(orm.get_contacts_in_group(group))
     app.contact.add_contact_to_group(contact.id, group)
-    group_after = list(orm.get_contacts_in_group(group))
-    group_before.append(contact)
-    assert sorted(group_before, key=Contact.id_or_max) == sorted(group_after, key=Contact.id_or_max)
+    contacts_in_group_after = list(orm.get_contacts_in_group(group))
+    contacts_in_group_before.append(contact)
+    assert sorted(contacts_in_group_before, key=Contact.id_or_max) == sorted(contacts_in_group_after, key=Contact.id_or_max)
 
 def test_delete_contact_from_group(app):
     if len(orm.get_group_list()) == 0:
@@ -27,8 +27,10 @@ def test_delete_contact_from_group(app):
         app.contact.create(Contact(firstname="NewFirstname", lastname="NewLastname"))
         contact = orm.get_contacts_not_in_group(group)[0]
         app.contact.add_contact_to_group(contact.id, group)
-    group_before = list(orm.get_contacts_in_group(group))
+    else:
+        contact = random.choice(orm.get_contacts_in_group(group))
+    contacts_in_group_before = list(orm.get_contacts_in_group(group))
     app.contact.remove_contact_from_group(contact.id, group)
-    group_after = list(orm.get_contacts_in_group(group))
-    group_before.remove(contact)
-    assert sorted(group_before, key=Contact.id_or_max) == sorted(group_after, key=Contact.id_or_max)
+    contacts_in_group_after = list(orm.get_contacts_in_group(group))
+    contacts_in_group_before.remove(contact)
+    assert sorted(contacts_in_group_before, key=Contact.id_or_max) == sorted(contacts_in_group_after, key=Contact.id_or_max)
